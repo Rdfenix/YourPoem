63ddfabed397bcb5a606dc50fbbb40349f09e1dd
Rdunei Oliveira <rhoodsk8erboy@gmail.com>
2022-02-27 12:06:10 -0300

    creating style to favorite card

--- src/component/poem/poem-card/index.tsx
+++ src/component/poem/poem-card/index.tsx
@@ -1,25 +1,47 @@
 import "./poem-card.css";
 import React from "react";
 
+import starIcon from "./../../../shared/assets/icon/star.png";
+
 interface Props {
   title?: string;
   author?: string;
   poem?: string;
+  favorite?: boolean;
 }
 
-const PoemCard = ({ author, title, poem }: Props) => {
+const PoemCard = ({ author, title, poem, favorite }: Props) => {
+  function openCardDetail() {
+    console.log("open detail");
+  }
+
+  function favoriteCard() {
+    console.log("favorite card");
+  }
+
+  const cssClassFavorite = favorite
+    ? "favorite-button favorite-card"
+    : "favorite-button";
+
   return (
     <div className="card-container">
-      <div className="card-header">
-        <div className="card-title">
-          <span>{title}</span>
-        </div>
-        <div className="card-author">
-          <span>{author}</span>
-        </div>
+      <div className="card-button-area">
+        <button className={cssClassFavorite} onClick={() => favoriteCard()}>
+          <img src={starIcon} alt="like" />
+        </button>
       </div>
-      <div className="card-body">
-        <span>{poem}</span>
+      <div className="card-wrapper" onClick={() => openCardDetail()}>
+        <div className="card-header">
+          <div className="card-title">
+            <span>{title}</span>
+          </div>
+          <div className="card-author">
+            <span>{author}</span>
+          </div>
+        </div>
+        <div className="card-body">
+          <span>{poem}</span>
+        </div>
       </div>
     </div>
   );
